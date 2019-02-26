5f8ea8f62624fb8d92cc8f20265858a0b530f657
Matt Morrison <mmorrison@broadsoft.com>
2019-02-26 22:27:29 +1300

    Add ability to read k8s config from stdin pipe

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -12,6 +13,8 @@ import (
 	"github.com/hashicorp/hcl2/hclwrite"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 // Command line flags
@@ -24,7 +27,7 @@ var (
 func init() {
 	// init command line flags
 	flag.BoolVar(&debug, "debug", false, "enable debug mode")
-	const inputUsage = `file or directory that contains the YAML configuration to convert.` // TODO: Use "-" to read from stdin`
+	const inputUsage = `file or directory that contains the YAML configuration to convert. Use "-" to read from stdin.`
 	flag.StringVar(&input, "filepath", "-", inputUsage)
 	flag.StringVar(&input, "f", "-", inputUsage+" (shorthand)")
 	// const outputUsage = `file or directory where Terraform config will be written`
@@ -48,6 +51,56 @@ func main() {
 }
 
 func readInput() []runtime.Object {
+	if input == "-" || input == "" {
+		return readStdinInput()
+	} else {
+		return readFilesInput()
+	}
+}
+
+func readStdinInput() []runtime.Object {
+	var objs []runtime.Object
+
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if info.Mode()&os.ModeCharDevice != 0 { //|| info.Size() <= 0 {
+		log.Fatalf("No data read from stdin")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	parsed, err := ParseK8SYAML(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, obj := range parsed {
+		log.Infof("%T", obj)
+		log.Infof("%s", obj.GetObjectKind())
+		if obj.GetObjectKind().GroupVersionKind().Kind == "List" {
+			list := obj.(*corev1.List)
+			for _, item := range list.Items {
+				itemObj, err := ParseK8SJSON(item.Raw)
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+				objs = append(objs, itemObj)
+
+			}
+
+		} else {
+			objs = append(objs, obj)
+
+		}
+	}
+
+	return objs
+}
+
+func readFilesInput() []runtime.Object {
 	var objs []runtime.Object
 
 	file, err := os.Open(input) // For read access.
