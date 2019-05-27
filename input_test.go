7d8fee8265593bbcc02a23b90fee7494dbae91a8
Matt Morrison <>
2019-05-27 12:23:12 +1200

    Suppress namespace.spec unsupported warning

--- input_test.go
+++ input_test.go
@@ -18,12 +18,12 @@ func Test_readFilesInput(t *testing.T) {
 		{
 			"test-fixtures",
 			"test-fixtures",
-			13,
+			14,
 		},
 		{
 			"test-fixtures/",
 			"test-fixtures/",
-			13,
+			14,
 		},
 		{
 			"test-fixtures/nested/server-clusterrole.yaml",
