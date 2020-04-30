a3d347194bcef45d3300b378234aff8329ac636f
Matt Morrison <matt@mojo.net.nz>
2020-04-30 12:04:19 +1200

    Update version to v0.4.1

--- Formula/k2tf.rb
+++ Formula/k2tf.rb
@@ -1,7 +1,7 @@
 class K2tf < Formula
   desc "Kubernetes YAML to Terraform HCL converter"
   homepage "https://github.com/sl1pm4t/k2tf"
-  url      "https://github.com/sl1pm4t/k2tf/archive/v0.4.0.tar.gz"
+  url      "https://github.com/sl1pm4t/k2tf/archive/v0.4.1.tar.gz"
   sha256   "d6070de2afc7bacf8a6ead24ad81b7d94f16e5106941ac78ced9b795ff4f8403"
   head     "https://github.com/sl1pm4t/k2tf.git"
 
