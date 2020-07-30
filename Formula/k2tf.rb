b02132ef30421dfc13ea2c0617fae2f371c800dd
Matt Morrison <matt@mojo.net.nz>
2020-07-30 15:27:55 +1200

    Update brew formula for v0.5.0

--- Formula/k2tf.rb
+++ Formula/k2tf.rb
@@ -1,8 +1,8 @@
 class K2tf < Formula
   desc "Kubernetes YAML to Terraform HCL converter"
   homepage "https://github.com/sl1pm4t/k2tf"
-  url      "https://github.com/sl1pm4t/k2tf/archive/v0.4.1.tar.gz"
-  sha256   "475478b3d8bb7b5af8201b3e512b78548875d93d88883bed1f52fc3e00bfac11"
+  url      "https://github.com/sl1pm4t/k2tf/archive/v0.5.0.tar.gz"
+  sha256   "94113ffb4874b9206148b7ea8bda56f30381b037547651e7c19af8547d845706"
   head     "https://github.com/sl1pm4t/k2tf.git"
 
   bottle :unneeded
